a0950a6911fbcace0f7a411a58dae82b30787342
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2022-09-15 17:10:57 +0600

    onBackPress and ProgressBar added

--- app/src/main/java/com/example/webviewandroid/MainActivity.java
+++ app/src/main/java/com/example/webviewandroid/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.webviewandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
 
         webView.loadUrl("https://www.google.com");
 
@@ -24,6 +29,29 @@ public class MainActivity extends AppCompatActivity {
 //            public boolean shouldOverrideUrlLoading(WebView view, String url) {
 //                return super.shouldOverrideUrlLoading(view, url);
 //            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
         });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
